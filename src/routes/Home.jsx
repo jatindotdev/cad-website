cbcf6cbdb00f739ec5d91368004f1cfcc1efa058
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2024-02-05 00:19:54 +0530

    ..

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,6 +5,7 @@ import About from "../sections/About/About";
 import Faq from "../sections/FAQ/Faq";
 
 function Home() {
+
   return (
     <>
       <section className="bg-[#200837] text-white">
@@ -13,13 +14,14 @@ function Home() {
         </section>
         <About/>
         <section className="overflow-hidden font-[vcr-osd] flex flex-col px-12 text-center py-20 bg-[#E2319A]">
-          <motion.div
-            className="flex text-6xl whitespace-nowrap uppercase mb-10 font-bold"
-            initial={{ x: -100 }}
-            animate={{ x: 100 }}
-            transition={{ duration: 3, repeat: 100, ease: "linear" }}>
-            / Exciting Challenges / Register Now /  / Unforgettable Experience 
-          </motion.div>
+        <motion.div
+        className="flex text-6xl whitespace-nowrap uppercase mb-10 font-bold"
+        initial={{ x: -100 }}
+        animate={{ x: 100 }}
+        transition={{ duration: 3, repeat: Infinity, ease: "linear" }}
+      >
+        / Exciting Challenges / Register Now / Unforgettable Experience /
+      </motion.div>
           <div className="flex flex-col justify-center items-center text-lg">
             <p className="mb-5">
               Registeration is now open for this thrilling 36 hour coding extravaganza !
