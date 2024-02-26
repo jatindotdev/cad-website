be3898dacfd2675f7abe233187b7840df1ccbc54
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2024-02-27 01:10:33 +0530

    ..

--- src/sections/Timeline/Timeline.jsx
+++ src/sections/Timeline/Timeline.jsx
@@ -1,147 +1,37 @@
 import React from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
 
 const Timeline = () => {
+  const timelineData = [
+    { date: "Feb 29", event: "DEVFOLIO Form Closes" },
+    { date: "Mar 1-7", event: "Ideation PPT Submission on Devfolio" },
+    { date: "Mar 8-11", event: "Filtration Round" },
+    { date: "Mar 11", event: "Filtration Results Announcement" },
+    { date: "Mar 14", event: "Introductory Ceremony" },
+    { date: "Mar 15 9:30 am", event: "Hacking Starts" },
+    { date: "Mar 16 9:30 pm", event: "Hacking Ends" },
+    { date: "Mar 17", event: "Prize Distribution Ceremony" },
+  ];
+
   return (
-    <div className="bg-[#0d0221] grid text-white-off py-10">
-      <h1 className="font-[Power] md:text-7xl text-white text-3xl font-semibold text-center mb-6 py-10">
+    <div className="bg-[#0d0221] text-white py-10 w-[100%]">
+      <h1 className="font-[Power] md:text-7xl text-white text-3xl font-semibold text-center mb-6">
         TIMELINE
       </h1>
-      <div className="md:mx-10 mx-5">
-        <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#FFFFFF" }}
-            contentArrowStyle={{ borderRight: "7px solid rgb(32, 8, 55) " }}
-            date="29th Feb"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              DEVFOLIO Form Closes
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(32, 8, 55)" }}
-            date="1st March - 7th March"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Ideation PPT Submission on Devfolio
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(32, 8, 55)" }}
-            date="8th March - 11th March"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Filtration Round
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(32, 8, 55)" }}
-            date="11th March"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Filtration Results Announcement
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid rgb(32, 8, 55)" }}
-            date="14th March"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Introductory Ceremony
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(32, 8, 55)" }}
-            date="15th March 9:30 am"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Hacking Starts
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(32, 8, 55)" }}
-            date="16th March 9:30 pm"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Hacking Ends
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            contentStyle={{ background: "rgb(32, 8, 55)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(32, 8, 55)" }}
-            date="17th March"
-            iconStyle={{ background: "rgb(285,1,157)", color: "#fff" }}
-            icon =""
-            >
-            <h3
-              className="vertical-timeline-element-title "
-              style={{ color: "white" }}>
-              Prize Distribution Ceremony <br /> BYE BYE CAD2.0!
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p></p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
+      <div className="md:mx-[35vw] mx-5 relative flex flex-col w-full my-20">
+        <div className="flex flex-col gap-4 justify-center items-start">
+          {timelineData.map((item, index) => (
+            <div key={index} className="flex flex-row items-center space-x-10 space-y-5">
+              <div className="md:block hidden text-white text-sm w-[5vw]">{item.date}</div>
+              <div className="md:w-16 md:h-16 h-5 w-5 flex items-center justify-center bg-[#f6019d] rounded-full">
+
+              </div>
+              <div className={`bg-[#200837] rounded-md p-4  ${index % 2 === 0 ? "ml-auto" : "mr-auto"}`}>
+                <h3 className="text-lg font-semibold capitalize">{item.event}</h3>
+              </div>
+            </div>
+          ))}
+         
+        </div>
       </div>
     </div>
   );
