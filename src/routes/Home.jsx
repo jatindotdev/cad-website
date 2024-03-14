6cedb92dec7b4f10a55a54023ea2eb5f1b80c702
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-03-14 09:06:07 +0530

    Fix animation

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,38 +1,37 @@
 import { motion } from "framer-motion";
-import Landing from "../sections/Landing/Landing";
 import Tracks from "../../components/Tracks";
 import About from "../sections/About/About";
 import Faq from "../sections/FAQ/Faq";
+import Landing from "../sections/Landing/Landing";
 import Timeline from "../sections/Timelines/Timeline";
 
 function Home() {
-
   return (
     <>
       <section className="bg-[#200837] text-white">
         <section className="min-h-screen font-[vcr-osd]">
           <Landing />
         </section>
-        <About/>
+        <About />
         <section className="overflow-hidden font-[vcr-osd] flex flex-col px-12 text-center py-20 bg-[#E2319A]">
-        <motion.div
-        className="flex text-6xl whitespace-nowrap uppercase mb-10 font-bold"
-        initial={{ x: -100 }}
-        animate={{ x: 100 }}
-        transition={{ duration: 3, repeat: Infinity, ease: "linear" }}
-      >
-        {/* / Exciting Challenges / Register Now / Unforgettable Experience / */}
-       / 1st Phase Review Results Out /  1st Phase Review Results Out /
-      </motion.div>
+          <motion.div
+            className="flex text-6xl whitespace-nowrap uppercase mb-10 font-bold"
+            animate={{ x: ["100%", "-200%"], y: 0 }}
+            transition={{
+              duration: 15,
+              repeat: Infinity,
+              ease: "linear",
+            }}>
+            {/* / Exciting Challenges / Register Now / Unforgettable Experience / */}
+            / 1st Phase Review Results Out / 1st Phase Review Results Out /
+          </motion.div>
           <div className="flex flex-col justify-center items-center text-lg">
-            <p className="mb-5">
-              1st Phase Review Results are now out 
-            </p>
+            <p className="mb-5">1st Phase Review Results are now out</p>
           </div>
         </section>
       </section>
       <Tracks />
-      <Timeline/>
+      <Timeline />
       <section className="bg-[#200837] text-white">
         <Faq className="min-h-screen" />
       </section>
